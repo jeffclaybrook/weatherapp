e9c4547d7a5abb45018eaed98c4024cff998ce7c
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2022-02-18 14:12:03 -0600

    Update request.js

--- request.js
+++ request.js
@@ -1,6 +1,6 @@
 const key = '1a3f145a3b10499787bc8baa36feed1e';
 
-// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=cbe3dd267a18f6c89943b3eff94f1ed7';
+// const baseURL = 'http://api.lopenweathermap.org/data/2.5/weather?q=Lagos&appid=1a3f145a3b10499787bc8baa36feed1e';
 
 // fetch(baseURL)
 //     .then((data) => { console.log('response', data.json()) })
@@ -9,7 +9,7 @@ const key = '1a3f145a3b10499787bc8baa36feed1e';
 //     });
 
 const requestCity = async (city) => {
-    const baseURL = 'http://api.openweathermap.org/data/2.5/weather'
+    const baseURL = 'https://api.openweathermap.org/data/2.5/weather'
     const query = `?q=${city}&appid=${key}`;
 
     //make fetch call (promise call)
